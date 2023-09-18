225bdb6c9001110fea9b965c533700d180644285
Ben Kogan <benkogan9@gmail.com>
2023-09-18 01:13:20 -0400

    1

--- bin/D1.ml
+++ bin/D1.ml
@@ -1,3 +1,19 @@
 open AOC.Aoc
 
-let () = List.iter (fun s -> print_endline s) (read_lines ())
+let fuel mass = (mass / 3) - 2
+
+let p1 =
+  read_lines ()
+  |> List.map (fun s -> s |> int_of_string |> fuel)
+  |> List.fold_left ( + ) 0
+
+let rec fuel_nested mass =
+  let fuel_val = fuel mass in
+  if fuel_val <= 0 then 0 else fuel_val + fuel_nested fuel_val
+
+let p2 =
+  read_lines ()
+  |> List.map (fun s -> s |> int_of_string |> fuel_nested)
+  |> List.fold_left ( + ) 0
+
+let () = Printf.printf "Part 1: %d\nPart 2: %d" p1 p2
